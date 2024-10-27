bf58515eb1a578f2eaf61d2e08e4798bff3bac06
Anna Brabec <ab.anna.brabec@gmail.com>
2024-10-27 20:33:38 +0100

    hotovo

---
+++ index.js
@@ -0,0 +1,12 @@
+const padlOrel = Math.random() < 0.5;
+const result = document.querySelector(".vysledek")
+const coin = document.querySelector(".mince")
+
+if (padlOrel) {
+    result.textContent = "Padl orel"
+    coin.classList.add("mince--orel")
+} else {
+    result.textContent = "Padla panna"
+    coin.classList.add("mince--panna")
+
+}
